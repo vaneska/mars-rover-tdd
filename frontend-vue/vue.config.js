737007c47639d5fdcbb6ddd07bfbb72b8adb6f13
2heoh <sergey.lobin@gmail.com>
2022-03-08 13:06:53 +0300

    use proper domain name

--- frontend-vue/vue.config.js
+++ frontend-vue/vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    proxy: "http://localhost:3000",
+    proxy: "http://rover:3000",
     port: 8000,
   },
 });
