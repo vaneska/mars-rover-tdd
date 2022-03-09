eb9138f7d6df980a8ed48cf7ba979c7cb3a319dc
Даниил Попов <d.popov@reg.ru>
2022-03-09 13:07:15 +0300

    пишем тесты

--- python-django/api/rover/views.py
+++ python-django/api/rover/views.py
@@ -1,8 +1,25 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 from rest_framework.views import APIView
+from dataclasses import asdict,dataclass
+import json
 
+@dataclass
+class Point:
+    x: int
+    y: int
+    direction: str
+
+class RoverGateway:
+    def move(self) -> Point:
+        return Point(x = 2, y = 3, direction = "N")
+
+    def rotate_left(self) -> Point:
+        pass
+
+    def rotate_right(self) -> Point:
+        pass
 
 class Commands(APIView):
 
@@ -11,5 +28,13 @@ class Commands(APIView):
         return HttpResponse("I'm a teapot", status=status.HTTP_418_IM_A_TEAPOT)
 
     @csrf_exempt
-    def commands(self):
-        return JsonResponse({ "point": { "x": 2, "y": 3, "direction": "N" }}, status=status.HTTP_201_CREATED)
+    def post(self, request: HttpRequest):
+        commands_str = json.loads(request.body).get("data")
+        array_of_commands = list(commands_str)
+        gw = RoverGateway()
+        for command in array_of_commands:
+            if command == "M": point = gw.move()
+            elif command == "L": point = gw.rotate_left()
+            elif command == "R": point = gw.rotate_right()
+
+        return JsonResponse({"point": asdict(point)}, status=status.HTTP_201_CREATED)
